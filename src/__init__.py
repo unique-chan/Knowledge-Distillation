cbbbbbc268c028511755fdc98634a7d44273575b
unique-chan <yechankim@gm.gist.ac.kr>
2021-11-28 00:15:23 +0900

    optimizer + lr_scheduler +

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 import torchvision.transforms as transforms
+import torch.optim as optim
 
 
 def get_manual_transform_list(mode, transform_list_name, mean, std):
@@ -21,3 +22,11 @@ def get_manual_transform_list(mode, transform_list_name, mean, std):
     if transform_list_dir.get(transform_list_name):
         transform_list = transform_list_dir[transform_list_name] + [transforms.Normalize(mean, std)]
     return transform_list
+
+
+def get_optimizer(model, lr):
+    return optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
+
+
+def get_lr_scheduler(optimizer, lr_step, lr_step_gamma):
+    return optim.lr_scheduler.MultiStepLR(optimizer, milestones=lr_step, gamma=lr_step_gamma)
