4677449b0f9781e1d6717ee9de7fd6744cc8757a
unique-chan <yechankim@gm.gist.ac.kr>
2021-11-28 01:44:31 +0900

    fix_random_seed +

--- src/util.py
+++ src/util.py
@@ -1,3 +1,8 @@
+import random
+from torch import manual_seed, cuda, backends
+import numpy as np
+
+
 class Meter:
     def __init__(self):
         self.values, self.avg, self.sum, self.cnt = [], 0, 0, 0
@@ -10,3 +15,12 @@ class Meter:
         self.sum += value
         self.cnt += k
         self.avg = self.sum / self.cnt
+
+
+def fix_random_seed(seed=1234):
+    # Ref.: https://github.com/bentrevett/pytorch-image-classification/blob/master/5_resnet.ipynb
+    random.seed(seed)
+    np.random.seed(seed)
+    manual_seed(seed)
+    cuda.manual_seed(seed)
+    backends.cudnn.deterministic = True
